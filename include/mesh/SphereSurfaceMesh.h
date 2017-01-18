2efadeae4ef615f16dd7fbb20734f60a99ef8ba3
Daniel Schwen <daniel.schwen@inl.gov>
2017-01-18 10:08:22 -0600

    Fix duplicate node issue in sphere surface mesh

--- include/mesh/SphereSurfaceMesh.h
+++ include/mesh/SphereSurfaceMesh.h
@@ -2,7 +2,6 @@
 #define SPHERESURFACEMESH_H
 
 #include "MooseMesh.h"
-#include <array>
 
 class SphereSurfaceMesh;
 
@@ -10,7 +9,8 @@ template <>
 InputParameters validParams<SphereSurfaceMesh>();
 
 /**
- *
+ * Create a sphere surface mesh based on the recursive subdovision of the faces
+ * of a regular icosahedron.
  */
 class SphereSurfaceMesh : public MooseMesh
 {
@@ -20,25 +20,31 @@ public:
 
   // No copy
   SphereSurfaceMesh & operator=(const SphereSurfaceMesh & other_mesh) = delete;
-
   virtual MooseMesh & clone() const override;
 
   virtual void buildMesh() override;
 
 protected:
+  // refine a triange into four triangles
   void subdivide(unsigned int v1, unsigned int v2, unsigned int v3, unsigned int depth);
+
+  // identify or add the midpoint of an edge given by two node indices
+  unsigned int midPoint(unsigned int a, unsigned int b);
 
   /// sphere radius
   const Real _radius;
 
-  /// number of nodes added
-  unsigned int _n_points;
-
-  /// iteration depth
+  /// recursion levels for triangle subdivision
   const unsigned int _depth;
 
-  /// underlying unstructured mesh
+  /// number of nodes added to keep track of node indices
+  unsigned int _n_points;
+
+  /// underlying unstructured mesh to build the sphere in
   UnstructuredMesh & _umesh;
+
+  // midpoints are shared between adjacent parent triangles, this map helps avoid adding duplicate nodes
+  std::map<std::pair<unsigned int, unsigned int>, unsigned int> _midpoint;
 };
 
 #endif // SPHERESURFACEMESH_H
