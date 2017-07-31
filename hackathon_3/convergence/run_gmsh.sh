75f428dc2f44b94ef58017df0da01664619590c5
Daniel Schwen <daniel.schwen@inl.gov>
2017-07-30 21:50:39 -0600

    save outputs

--- hackathon_3/convergence/run_gmsh.sh
+++ hackathon_3/convergence/run_gmsh.sh
@@ -22,7 +22,7 @@ fi
 #
 
 cat $BASE.i convergence.inc > ${BASE}_convergence.i
-rm problem1b_convergence.csv
+rm ${BASE}_convergence.csv
 for scale in 0.11 0.15 0.2 0.3 0.5 0.75 1.0 1.2 1.5 2 3 4 5 8 10 14
 do
   cd mesh
@@ -30,6 +30,11 @@ do
   cd ..
 
   mpirun -n 4 ../../CHiMaD_Hackathon-opt -i ${BASE}_convergence.i UserObjects/reference/mesh=$REF
+
+  OUT=outputs/${BASE}_$scale
+  mkdir -p $OUT
+  mv ${BASE}_convergence_out* $OUT
+
 
   grep '^1,' ${BASE}_convergence_out.csv | cut -d, -f2- >>${BASE}_convergence.csv
 done
--- hackathon_3/convergence/run_moose.sh
+++ hackathon_3/convergence/run_moose.sh
@@ -8,7 +8,7 @@ BASE=problem1b_moose
 REF=${BASE}_reference.e
 if [ ! -s $REF ]
 then
-  ny=160
+  ny=320
   # 1128502 DOFs
   mpirun -n 12 ../../CHiMaD_Hackathon-opt -i $BASE.i Mesh/ny=$ny Mesh/nx=$(expr $ny \* 5)
   mv ${BASE}_out.e $REF
@@ -19,9 +19,14 @@ fi
 #
 
 cat $BASE.i convergence.inc > ${BASE}_convergence.i
-for ny in 80 60 40 30 20 15 10 8 6
+rm ${BASE}_convergence.csv
+for ny in 160 120 80 60 40 20 10 8 
 do
   mpirun -n 4 ../../CHiMaD_Hackathon-opt -i ${BASE}_convergence.i Mesh/ny=$ny Mesh/nx=$(expr $ny \* 5) UserObjects/reference/mesh=$REF
 
+  OUT=outputs/${BASE}_$ny
+  mkdir -p $OUT
+  mv ${BASE}_convergence_out* $OUT
+  
   grep '^1,' ${BASE}_convergence_out.csv | cut -d, -f2- >>${BASE}_convergence.csv
 done
